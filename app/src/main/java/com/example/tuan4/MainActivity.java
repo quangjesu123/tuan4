69770dddca2b65e7819b54ad9b8c3373a9f3fd8b
Nguyễn Đức Quang <quangnguyenduc4@gmail.com>
2019-05-13 14:25:14 +0700

    Initial commit

---
+++ app/src/main/java/com/example/tuan4/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.tuan4;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+
+public class MainActivity extends AppCompatActivity {
+//    CheckBox cau1;
+//    CheckBox cau2;
+//    CheckBox cau3;
+//    CheckBox cau4;
+//    Button back;
+//    Button result;
+//    Button next;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        cau1.findViewById(R.id.ck_1);
+//        cau2.findViewById(R.id.ck_2);
+//        cau3.findViewById(R.id.ck_3);
+//        cau4.findViewById(R.id.ck_4);
+//        back.findViewById(R.id.back);
+//        result.findViewById(R.id.result);
+//        next.findViewById(R.id.next);
+
+
+    }
+}
